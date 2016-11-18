5e0cb5099b2ddb3d52e53895700de1987c373e51
duco.fronik <duco.fronik@capgemini.com>
2016-11-18 22:00:34 +0100

    Quesdtion answer tryout

---
+++ Appje/ProgrammingSupport/ProgrammingSupport.Core/Business/StackAnswerer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingSupport.Core.Business
+{
+    public static class StackAnswerer
+    {
+        public static async Task<string> AnswerMe(string question)
+        {
+            //Difficult shit here
+            return "Oh no Kenny Died!";
+        }
+
+    }
+}
--- Appje/ProgrammingSupport/ProgrammingSupport.Droid/Views/FirstView.cs
+++ Appje/ProgrammingSupport/ProgrammingSupport.Droid/Views/FirstView.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using MvvmCross.Droid.Views;
 using Android.Webkit;
 using Android.Views;
+using ProgrammingSupport.Core.ViewModels;
 
 namespace ProgrammingSupport.Droid.Views
 {
@@ -10,6 +11,11 @@ namespace ProgrammingSupport.Droid.Views
     public class FirstView : MvxActivity
     {
 		private WebView _webView;
+
+        private FirstViewModel FirstViewModel
+        {
+            get { return (ViewModel as FirstViewModel); }
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -25,6 +31,8 @@ namespace ProgrammingSupport.Droid.Views
 			//_webView.LoadDataWithBaseURL(null, "<html><body style=\"width: 100%; height:100%; margin:0px; padding:0px;\">\n<img style=\"width: 100%; height:100%; margin:0px; padding:0px;\" id=\"selector\" src=\"https://media.giphy.com/media/1iUlZloL4DiH8HL2/giphy.gif\"></body></html>", "text/html", "utf-8", null);
 			_webView.Settings.JavaScriptEnabled = true;
 			_webView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+            //FirstViewModel.GoToAnswerCommand.Execute(null);
         }
     }
 }
