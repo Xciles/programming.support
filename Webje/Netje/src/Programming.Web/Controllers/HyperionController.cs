272093dff6fa391dbacf924e4063a799e37c92fa
Hans Harts <hans.harts@capgemini.com>
2016-11-18 21:58:42 +0100

    Default controller

---
+++ Webje/Netje/src/Programming.Web/Controllers/HyperionController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming.Web.Controllers
+{
+    public class HyperionController
+    {
+    }
+}
