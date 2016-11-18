24852ece8fd321a6c979dcf19550423e8ca6ca88
Arjan Nieuwenhuis <arjan.nieuwenhuis@capgemini.com>
2016-11-18 22:30:28 +0100

    Remove Html tags

--- Webje/Botje/Programming.Bot/Business/StackOverflow.cs
+++ Webje/Botje/Programming.Bot/Business/StackOverflow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
@@ -40,9 +41,15 @@ namespace Programming.Bot.Business
             jsonDataResponse = await msg.Content.ReadAsStringAsync();
             var answerData = JsonConvert.DeserializeObject<AnswerResult>(jsonDataResponse);
 
-            //TODO add html cleaner
-            return answerData.items[0].body;
+            return RemoveHtmlTags(answerData.items[0].body);
+        }
 
+        public static string RemoveHtmlTags(string html)
+        {
+            if (html == null) return string.Empty;
+            var response = Regex.Replace(html, @"<[^>]+>|&nbsp;", "").Trim();
+            response = response.Replace("&amp;", "&");
+            return response;
         }
     }
 }
