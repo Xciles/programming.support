26597a1a3570c4f114f642e4f38826656f44b60b
Hans Harts <hans.harts@capgemini.com>
2016-11-18 23:44:55 +0100

    Adding So api to api !

--- Webje/Netje/src/Programming.Web/Controllers/HyperionController.cs
+++ Webje/Netje/src/Programming.Web/Controllers/HyperionController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Programming.Web.Controllers
 {
-    public class HyperionController
+    [Route("api/hyperion")]
+    public class HyperionController : Controller
     {
+        [HttpPost()]
+        public string Get(int id)
+        {
+            return "value";
+        }
     }
 }
