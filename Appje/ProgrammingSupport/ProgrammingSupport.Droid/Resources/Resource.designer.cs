05dc7d41f21806dcbb33970335205deeaba3bcf7
duco.fronik <duco.fronik@capgemini.com>
2016-11-18 23:12:49 +0100

    SkypeView

--- Appje/ProgrammingSupport/ProgrammingSupport.Droid/Resources/Resource.designer.cs
+++ Appje/ProgrammingSupport/ProgrammingSupport.Droid/Resources/Resource.designer.cs
@@ -134,7 +134,10 @@ namespace ProgrammingSupport.Droid
 			public const int FirstView = 2130968577;
 			
 			// aapt resource value: 0x7f040002
-			public const int SplashScreen = 2130968578;
+			public const int SkypeView = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int SplashScreen = 2130968579;
 			
 			static Layout()
 			{
