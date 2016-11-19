a7f9fd593a5f23f1c779e00351c812d1249645ec
duco.fronik <duco.fronik@capgemini.com>
2016-11-19 01:01:54 +0100

    pizza start

--- Appje/ProgrammingSupport/ProgrammingSupport.Droid/Resources/Resource.designer.cs
+++ Appje/ProgrammingSupport/ProgrammingSupport.Droid/Resources/Resource.designer.cs
@@ -134,10 +134,13 @@ namespace ProgrammingSupport.Droid
 			public const int FirstView = 2130968577;
 			
 			// aapt resource value: 0x7f040002
-			public const int SkypeView = 2130968578;
+			public const int PizzaView = 2130968578;
 			
 			// aapt resource value: 0x7f040003
-			public const int SplashScreen = 2130968579;
+			public const int SkypeView = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int SplashScreen = 2130968580;
 			
 			static Layout()
 			{
